ec69c65111cbba114902670039a3d85aa44818cd
Christina Manara <manara1024@gmail.com>
2021-05-27 13:43:14 +0300

    changes_on_911.py

--- 911.py
+++ 911.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
+import seaborn as sns1
+import matplotlib. pyplot as plt
+sns.set_style('whitegrid')
 
 
 def calls_911():
@@ -22,7 +25,26 @@ def calls_911():
     # What is the most common Reason for a 911 call based off of this new column?
     print(df['Reason'].value_counts())
     # Now use seaborn to create a count plot of 911 calls by Reason.
-
+    sns.countplot(x='Reason', data=df, palette='viridis')
+    plt.show()
+    # Now let us begin to focus on time information. What is the data type of the objects in the timeStamp column?
+    var = df['timeStamp'].iloc[0]
+    print(type(var))
+    print(pd.to_datetime(df['timeStamp']))
+    df['timeStamp'] = pd.to_datetime(df['timeStamp'])
+    df['Hour'] = df['timeStamp'].apply(lambda h: h.hour)
+    df['Month'] = df['timeStamp'].apply(lambda m: m.month)
+    df['Day of Week'] = df['timeStamp'].apply(lambda w: w.dayofweek)
+    dmap = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
+    df['Day of Week'] = df['Day of Week'].map(dmap)
+    # Now use seaborn to create a countplot of the Day of Week column with the hue based off of the Reason column.
+    sns1.countplot(x='Day of Week', hue='Reason', data=df, palette='viridis')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
+    sns.countplot(x='Month', data=df, hue='Reason', palette='viridis')
+    # To relocate the legend
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
 
 
 if __name__ == '__main__':
