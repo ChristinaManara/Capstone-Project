8331fb569cd5b7f2d007818d133faf37d42f868d
Christina Manara <manara1024@gmail.com>
2021-05-30 13:55:46 +0300

    changes_on_911.py

--- 911.py
+++ 911.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import seaborn as sns1
@@ -45,6 +44,43 @@ def calls_911():
     # To relocate the legend
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.show()
+    # Now create a gropuby object called byMonth, where you group the DataFrame by the month column and use the
+    # count() method for aggregation. Use the head() method on this returned DataFrame.
+    byMonth = df.groupby('Month').count()
+    print(byMonth)
+    # Now create a simple plot off of the dataframe indicating the count of calls per month.
+    byMonth['twp'].plot()
+    plt.show()
+    # Now see if you can use seaborn's lmplot() to create a linear fit on the number of calls per month.
+    # Keep in mind you may need to reset the index to a column.
+    sns.lmplot(x='Month', y='twp', data=byMonth.reset_index())
+    plt.show()
+    # Create a new column called 'Date' that contains the date from the timeStamp column.
+    # You'll need to use apply along with the .date() method.
+    df['Date'] = df['timeStamp'].apply(lambda data: data.date())
+    print(df['Date'])
+    # Now groupby this Date column with the count() aggregate and create a plot of counts of 911 calls.
+    df.groupby('Date').count()['twp'].plot()
+    plt.tight_layout()
+    plt.show()
+    # Now recreate this plot but create 3 separate plots with each plot representing a Reason for the 911 call
+    df[df['Reason'] == 'Traffic'].groupby('Date').count()['twp'].plot()
+    plt.tight_layout()
+    plt.title('Traffic')
+    plt.show()
+    df[df['Reason'] == 'EMS'].groupby('Date').count()['twp'].plot()
+    plt.tight_layout()
+    plt.title('EMS')
+    plt.show()
+    df[df['Reason'] == 'Fire'].groupby('Date').count()['twp'].plot()
+    plt.tight_layout()
+    plt.title('Fire')
+    plt.show()
+    # Now let's move on to creating heatmaps with seaborn and our data. We'll first need to restructure the dataframe
+    # so that the columns become the Hours and the Index becomes the Day of the Week. There are lots of ways to do
+    # this, but I would recommend trying to combine groupby with an unstack method. Reference the solutions if you
+    # get stuck on this!
+
 
 
 if __name__ == '__main__':
